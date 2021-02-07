d16fd103a3e6b2daa4d5a471ff0a536e1b714b55
Chris Joyce <chris@Chriss-iMac.modem>
2021-02-08 08:40:05 +1000

    custom event toggle modle with close

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <h1>{{ title }}</h1>
   <p>Welcome...</p>
   <div v-if="showModal">
-    <Modal :heading="heading" :text="text" theme="sale" />
+    <Modal :heading="heading" :text="text" theme="sale" @close="toggleModal" />
   </div>
   <button @click="toggleModal">open modal</button>
 </template>
