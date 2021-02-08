ac79cfb38790bc26a4e84ceb9853e2959bbb4918
Chris Joyce <chris@Chriss-iMac.modem>
2021-02-08 15:01:44 +1000

    First use of teleport

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <h1>{{ title }}</h1>
   <p>Welcome...</p>
 
-  <div v-if="showModal">
+  <teleport to="#modals" v-if="showModal">
     <Modal theme="sale" @close="toggleModal">
       <template v-slot:links>
         <a href="#">sign up now</a>
@@ -11,14 +11,14 @@
       <h1>Ninja Givaway!</h1>
       <p>Grab your ninja swag for half price!</p>
     </Modal>
-  </div>
+  </teleport>
 
-  <div v-if="showModalTwo">
+  <teleport to="#modals" v-if="showModalTwo">
     <Modal @close="toggleModalTwo">
       <h1>Sign up to the Newsletter</h1>
       <p>For updates and promo codes!</p>
     </Modal>
-  </div>
+  </teleport>
 
   <button @click.alt="toggleModal">open modal (alt click)</button>
   <button @click="toggleModalTwo">open modal 2</button>
@@ -48,7 +48,8 @@ export default {
 </script>
 
 <style>
-#app {
+#app,
+#modals {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
