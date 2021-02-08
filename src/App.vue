8a1020dcf62a65b7fa422a528aa99909a061b7b3
Chris Joyce <chris@Chriss-iMac.modem>
2021-02-08 12:38:32 +1000

    Re-using same component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,27 @@
 <template>
   <h1>{{ title }}</h1>
   <p>Welcome...</p>
+
   <div v-if="showModal">
     <Modal theme="sale" @close="toggleModal">
+      <template v-slot:links>
+        <a href="#">sign up now</a>
+        <a href="#">more info</a>
+      </template>
       <h1>Ninja Givaway!</h1>
       <p>Grab your ninja swag for half price!</p>
     </Modal>
   </div>
-  <button @click.alt="toggleModal">open modal (alt - option)</button>
+
+  <div v-if="showModalTwo">
+    <Modal @close="toggleModalTwo">
+      <h1>Sign up to the Newsletter</h1>
+      <p>For updates and promo codes!</p>
+    </Modal>
+  </div>
+
+  <button @click.alt="toggleModal">open modal (alt click)</button>
+  <button @click="toggleModalTwo">open modal 2</button>
 </template>
 
 <script>
@@ -18,14 +32,16 @@ export default {
   data() {
     return {
       title: "My First Vue App!",
-      heading: "Sign up for the Givaway!",
-      text: "Grab your ninja swag for half price!",
       showModal: false,
+      showModalTwo: false,
     };
   },
   methods: {
     toggleModal() {
       this.showModal = !this.showModal;
+    },
+    toggleModalTwo() {
+      this.showModalTwo = !this.showModalTwo;
     },
   },
 };
@@ -44,5 +60,14 @@ h1 {
   border-bottom: 1px solid #ddd;
   display: inline-block;
   padding-bottom: 10px;
+}
+button {
+  background: #bbb;
+  padding: 10px;
+  border: none;
+  border-radius: 4px;
+  margin: 10px;
+  font-size: 16px;
+  color: #333;
 }
 </style>
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="backdrop" @click.self="closeModal">
     <div class="modal" :class="{ sale: theme === 'sale' }">
-      <slot></slot>
+      <slot>default content (if no slot passed in)</slot>
+      <div class="actions">
+        <slot name="links"></slot>
+      </div>
     </div>
   </div>
 </template>
@@ -9,7 +12,6 @@
 <script>
 export default {
   props: ["theme"],
-
   methods: {
     closeModal() {
       this.$emit("close");
@@ -17,6 +19,7 @@ export default {
   },
 };
 </script>
+
 <style>
 .modal {
   width: 400px;
@@ -40,13 +43,31 @@ export default {
 .modal p {
   font-style: normal;
 }
-
+.modal .actions {
+  text-align: center;
+  margin: 30px 0 10px 0;
+  color: #333;
+}
+.modal .actions a {
+  color: #333;
+  padding: 8px;
+  border: 1px solid #eee;
+  border-radius: 4px;
+  text-decoration: none;
+  margin: 10px;
+}
 /* sale styles */
 .modal.sale {
   background: crimson;
   color: white;
 }
 .modal.sale h1 {
+  color: white;
+}
+.modal.sale .actions {
+  color: white;
+}
+.modal.sale .actions a {
   color: white;
 }
 </style>
